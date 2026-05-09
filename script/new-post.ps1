717e5d8417698c1fb6f0fe3fb9ee674e05fe603c
watako <778769386.qq.com>
2026-05-09 20:43:19 +0800

    更新于 2026-05-09 20:43

---
+++ script/new-post.ps1
@@ -0,0 +1,14 @@
+﻿$blogRoot = "E:\blog\my-blog"   # 改成你的纯英文博客路径
+$newFile = "$blogRoot\content\new-post.md"
+
+$template = @"
+---
+title: "新文章"
+date: $(Get-Date -Format 'yyyy-MM-ddTHH:mm:ss+08:00')
+draft: false
+---
+
+"@
+
+[System.IO.File]::WriteAllText($newFile, $template, [System.Text.UTF8Encoding]::new($true))
+Start-Process $newFile
---
+++ script/publish.ps1
@@ -0,0 +1,7 @@
+﻿$blogRoot = "E:\blog\my-blog"
+Set-Location $blogRoot
+hugo
+git add .
+git commit -m "更新于 $(Get-Date -Format 'yyyy-MM-dd HH:mm')"
+git push origin main
+Write-Host "发布完成！" -ForegroundColor Green
